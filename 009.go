015b87c46897c8d72ff2652f5663f89a85df4616
Kouta Asai <scene.4913@gmail.com>
2017-03-21 23:55:12 +0900

    Fix typo

--- 009.go
+++ 009.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func main() {
 	n := 1000
-	largetst := 0
-	for a := 0; a < 1000; a++ {
+	largest := 0
+	for a := 0; a < n/2; a++ {
 		b := (2*a*n - n*n) / (2*a - 2*n)
 		c := n - a - b
 		if a*a+b*b == c*c {
-			if num := a * b * c; largetst < num {
-				largetst = num
+			if num := a * b * c; largest < num {
+				largest = num
 			}
 		}
 	}
-	fmt.Println(largetst)
+	fmt.Println(largest)
 }
