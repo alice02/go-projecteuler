87fae363c66ca1fe126f59d47adbaded4f0d0c83
Kouta Asai <scene.4913@gmail.com>
2017-03-22 23:27:33 +0900

    Add problem 010

---
+++ 010.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func InitializeFlag(number int) []bool {
+	array := make([]bool, number)
+	for i := range array {
+		array[i] = true
+	}
+	array[0] = false
+	array[1] = false
+	return array
+}
+
+func EratosthenesSieve(number int) []int {
+	primes := []int{}
+	flags := InitializeFlag(number)
+	for i := 2; i < int(math.Sqrt(float64(number))); i++ {
+		if flags[i] {
+			for j := i * i; j < number; j += i {
+				flags[j] = false
+			}
+		}
+	}
+	for i := 0; i < number; i++ {
+		if flags[i] {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func CalcuateArraySum(array []int) int {
+	sum := 0
+	for _, i := range array {
+		sum += i
+	}
+	return sum
+}
+
+func main() {
+	primes := EratosthenesSieve(2000000)
+	fmt.Println(CalcuateArraySum(primes))
+}
