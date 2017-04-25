6ce36f239b9ad0132aefda6f7429cd42a805fac4
Kouta Asai <scene.4913@gmail.com>
2017-04-25 23:31:39 +0900

    Add problem30 test

--- Problem030/030_test.go
+++ Problem030/030_test.go
@@ -1,11 +1,22 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestProblem030(t *testing.T) {
 	actual := Problem030()
 	expected := 443839
 	if actual != expected {
+		t.Errorf("got %v want %v", actual, expected)
+	}
+}
+
+func TestSplitDigits(t *testing.T) {
+	actual := SplitDigits(1234)
+	expected := []int{1, 2, 3, 4}
+	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("got %v want %v", actual, expected)
 	}
 }
