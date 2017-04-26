05f6437041532ef169cb9d80f9d735e6073f6b58
Kouta Asai <scene.4913@gmail.com>
2017-04-26 23:57:26 +0900

    Add problem 031 (it not works)

---
+++ Problem031/031.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Problem030() int {
+	coins := []int{200, 100, 50, 20, 10, 5, 2, 1}
+	target := 200
+	return target
+}
+
+func main() {
+	fmt.Println(Problem030())
+}
